ad668583578e5fa1e6f023db657a336b506cac85
Moritz Asche <moritz@asches.de>
2021-02-25 21:35:41 -0500

    I now have a dot in the center of the view, and it changes colour with an animation. Going to work on making that ball bounce, and change colour when it hits the ground.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -7,14 +7,39 @@
 
 import SwiftUI
 
+
+
+
 struct CustomComposableView: View {
+    
+    @State private var shouldAnimate = true
+    @State private var circleColour = Color.green
+    @State var topOffset: CGFloat = 500
+    @State var bottomOffset: CGFloat = 0
+    @State var bottom: CGPoint = 
+    let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(circleColour)
+            .frame(width: 40, height: 40)
+            .offset(x: shouldAnimate ? bottomOffset : topOffset)
+            .animation(Animation
+                        .easeOut(duration: 2)
+                        .repeatForever(autoreverses: true))
+//            .onAppear() {
+//                shouldAnimate = true
+//                circleColour = .red
+//            }
+            .accessibility(activationPoint: bottom)
+        
     }
+    
 }
+
 
 struct CustomComposableView_Previews: PreviewProvider {
     static var previews: some View {
         CustomComposableView()
     }
 }
+
